9cbdfdcd0662c17b89214da0fb7cc34a2a94397e
Benjamin Post <whoisbenjaminpost@gmail.com>
2023-01-08 20:47:50 +0100

    inventoory - clean up

--- 2022_12/NODEJS/inventoory/inventoory.js
+++ 2022_12/NODEJS/inventoory/inventoory.js
@@ -14,70 +14,33 @@ server.listen(port,hostname, () => {
   console.log(`Server is Listening on ${hostname}:${port}`)})
 
 // create SQLITE DB
-let db = new sqlite3.Database('./db/sample.db', (err) => {
+// './db/sample.db'
+let db = new sqlite3.Database(':memory:', (err) => {
     if (err) {
       return console.error(err.message);
     }
     console.log('Connected to the sample SQlite database.');
   });
 
+db.run('CREATE TABLE hello (name TEXT)')
+
 // CREATE SQLITE TABLE
 db.run('CREATE TABLE IF NOT EXISTS \
   inventory(id INTEGER PRIMARY KEY, name TEXT, typ TEXT, Neupreis TEXT, Ort TEXT)');
-
-
-// TRY EDITING SQLITE DB:
-console.log("try inputting a value:")
-// db.run("INSERT or REPLACE INTO inventory (name, typ) VALUES ('hello', 'bar')");
 
 ////////////////////////////////////////////////////////////////////////////////////
 //(Beispiel-)Daten aus JSON-Datei laden
 let rawData = fs.readFileSync('./data/items.json')
 let items = JSON.parse(rawData)
-// let items;
-// fs.readFile('./data/items.json', (err, data) => {
-//     items = JSON.parse(data);
-// })
-console.log("ITEMS")
-// for (let i=0; i<10; i++) {
-//     console.log(JSON.stringify(items[i]))
-// }
-// let myObj = {
-//     name: "Auto",
-//     typ: "Fahrzeug",
-//     Neupreis : "3000",
-//     Ort: "Garage",
-// };
-// let sqlQuery = 'INSERT INTO inventory VALUES(NULL, :name, :typ, :Neupreis, :Ort);';
 
-// Run command and variable will be replaces
-//db.run(sqlQuery, [myObj.name, myObj.typ, myObj.Neupreis, myObj.Ort])
-
+// INSTERT DEFAULT VALUES INTO DATABASE
 let sqlQuery = 'INSERT INTO inventory VALUES(NULL, :name, :typ, :Neupreis, :Ort);';
 
 for (var i=0; i<items.length; i++) {
   let item = items[i];
-  console.log(item)
   db.run(sqlQuery, [item.name, item.typ, item.Neupreis, item.Ort])
 }
 
-// db.run("INSERT or REPLACE INTO inventory (name, typ) VALUES (?, 'bar')");
-
-// db.run("INSERT INTO inventory VALUES "+
-//     "json_extract(value, '$.name'), " +
-//     "json_extract(value, '$.type') " +
-//     "FROM json_each(?);", (items));
-
-// INSERT INTO my_table SELECT 
-//   json_extract(value, '$.uri'), 
-//   json_extract(value, '$.user_agent')
-// FROM json_each(readfile('my_data.json'));
-
-// let stmt = db.prepare('INSERT INTO inventory VALUES(json(?))');
-//     for (let i=0; i<10; i++) {
-//         stmt.run(JSON.stringify({ a: i}));
-//     }
-//     stmt.finalize();
 
 console.log("AFTERWARDS")
 
